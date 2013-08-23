38cba061d17b436a5ebb6d294f447fddfec230e9
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-08-23 14:26:33 -0700

    scripts: setup passwordless sudo for Vagrant

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# Set up sudo
+echo %vagrant ALL=NOPASSWD:ALL > /etc/sudoers.d/vagrant
+chmod 0440 /etc/sudoers.d/vagrant
+
 # Setup sudo to allow no-password sudo for "sudo"
 usermod -a -G sudo vagrant
 
