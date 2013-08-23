9ff2537df70cb644a9ad2ff1e685ec4db47a918a
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-08-23 12:04:12 -0700

    scripts: remove the exit from vm_cleanup so it'll work

--- scripts/vm_cleanup.sh
+++ scripts/vm_cleanup.sh
@@ -13,7 +13,6 @@ rm /lib/udev/rules.d/75-persistent-net-generator.rules
 
 echo "Adding a 2 sec delay to the interface up, to make the dhclient happy"
 echo "pre-up sleep 2" >> /etc/network/interfaces
-exit
 
 # Zero out the free space to save space in the final image:
 dd if=/dev/zero of=/EMPTY bs=1M
